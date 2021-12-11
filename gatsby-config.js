eb2ccc59750c7b551af7565f79d0d1ed48ce4083
Daniel Tossou <tossdaniel@gmail.com>
2021-12-11 15:29:10 +0000

    Added gatsby-plugin-image plugins and made its config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,24 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "portfolio_v2",
   },
-  plugins: ["gatsby-plugin-postcss"],
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+  ],
 };
